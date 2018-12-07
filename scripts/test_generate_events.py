810cbbb8ed50df07f1dfed77e8063a4e6415d5c7
AndreiSavochkin <savochkin.andrei2010@yandex.ru>
2018-12-07 15:10:01 +0000

    Script

--- scripts/test_generate_events.py
+++ scripts/test_generate_events.py
@@ -2,7 +2,29 @@ import json
 import random
 
 while True:
-	a={}
-	a['value']=random.randint(1,5)
-	a['sensor']=random.randint(-40,40)
-	print(json.dumps(a))
+        a={}
+        a['value']='temperature'
+        a['sensor']=random.randint(-40,40)
+        print(json.dumps(a))
+        a['value']='pressure'
+        a['sensor']=random.randint(730,760)
+        print(json.dumps(a))
+        a['value']='illumination'
+        a['sensor']=random.randint(-40,40)
+        print(json.dumps(a))
+        a['value']='humidity'
+        a['sensor']=random.randint(-40,40)
+        print(json.dumps(a))
+        a['value']='noise'
+        a['sensor']=random.randint(-40,40)
+        print(json.dumps(a))
+        a['value']='geger'
+        a['sensor']=random.randint(0,210)
+        print(json.dumps(a))
+        a['value']='gases'
+        a['sensor']=random.randint(-40,40)
+        print(json.dumps(a))
+
+
+
+
